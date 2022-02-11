feb9e2d8b68fe87b318a269fd792e9d0fff3f254
Ashitosh S Kadam <95925635+ashitoshk@users.noreply.github.com>
2022-02-11 22:05:10 +0530

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout<<"Hello World";
+
+    int i =0;
+    while(i<4)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+
+    return 0;
+}
